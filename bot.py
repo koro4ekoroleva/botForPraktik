e8814bae4919253b2af5b392e3c3b93ab24391d7
Liza <ElizavetkaKuzina@yandex.ru>
2024-07-08 21:19:09 +0300

    Теперь товары перелистываются

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 import logging
 from io import BytesIO
 
-bot = telebot.TeleBot('6782288362:AAGOEAPSZQc-2MzwnBDcRfzpg5vX9YhGk4k', skip_pending=True)
+bot = telebot.TeleBot('7397593743:AAGJXf5jUKLO7EsoDTf2W8nVNmj68RbBnTs', skip_pending=True)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
@@ -63,17 +63,22 @@ def where_order_callback(callback):
     bot.send_message(callback.message.chat.id, text='Ваш заказ был отдан на нужды голодающих африканских детей!'
                                                     '\n/start - начать сначала')
 
+viewed_product = 0
 
 @bot.callback_query_handler(func=lambda callback: 'category' in callback.data)
-def products_callback(callback, viewed_product = 0):
+def products_callback(callback):
+    global viewed_product, row
     category = callback.data[8:]
-    query = (f'SELECT products.products_id, products.product_name, maker.maker_name, maker.maker_country, ' 
-             f'products.price, products.picture FROM products JOIN maker ON products.maker_id = maker.maker_id '
-             f'WHERE products.category_id = {category}')
-    row = connect(query)
     print(viewed_product)
-    if viewed_product < 0:
+    if viewed_product == 0 :
+        query = (f'SELECT products.products_id, products.product_name, maker.maker_name, maker.maker_country, '
+                 f'products.price, products.picture FROM products JOIN maker ON products.maker_id = maker.maker_id '
+                 f'WHERE products.category_id = {category}')
+        row = connect(query)
+    elif viewed_product < 0:
         viewed_product = 0
+    elif viewed_product >= len(row):
+        viewed_product = len(row) - 1
     pictures = list()
     for i in range(len(row)):
         pictures.append([row[i][0], row[i][5]])
@@ -82,21 +87,24 @@ def products_callback(callback, viewed_product = 0):
                         f'\n\n*Цена: {row[viewed_product][4]} ₽*')
     photo_now = BytesIO(pictures[viewed_product][1])
 
-    markup = telebot.types.InlineKeyboardMarkup(row_width=3)
+    markup = telebot.types.InlineKeyboardMarkup(row_width=2)
     product_before = telebot.types.InlineKeyboardButton(text='\U00002B05', callback_data='product_before')
-    cart = telebot.types.InlineKeyboardButton(text='В корзину\U0001F6D2\u2002', callback_data='cart')
+    cart = telebot.types.InlineKeyboardButton(text='Купить\U0001F6D2\u2002', callback_data='cart')
     product_after = telebot.types.InlineKeyboardButton(text='\U000027A1', callback_data='product_after')
-    markup.add(product_before, cart, product_after)
+    markup.add(product_before, product_after)
+    markup.add(cart)
     bot.send_photo(callback.message.chat.id, caption=text_for_message, photo=photo_now, parse_mode='Markdown',
                    reply_markup = markup)
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == 'product_before' or callback.data == 'product_after')
 def products_callback_next(callback):
+    global viewed_product
     if callback.data == 'product_before':
-        products_callback(callback, viewed_product = +1)
+        viewed_product -= 1
     elif callback.data == 'product_after':
-        products_callback(callback, viewed_product=-1)
+        viewed_product += 1
+    products_callback(callback)
 
 @bot.message_handler(content_types=["text"])
 def what(message):
